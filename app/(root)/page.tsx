5a23dde7173c8362d69264a4f442e8e2bed87a2a
Sham Alahmad <112821170+shamsa19@users.noreply.github.com>
2026-02-06 15:24:50 +0300

    Add auth UI, social buttons, and icon assets

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,29 +10,7 @@ import {
 } from "@/components/ui/dropdown-menu"
 
 const page = () => {
-  return (
-    <>
-      <div>page</div>
-      <DropdownMenu>
-  <DropdownMenuTrigger asChild>
-    <Button variant="outline">Open</Button>
-  </DropdownMenuTrigger>
-  <DropdownMenuContent>
-    <DropdownMenuGroup>
-      <DropdownMenuLabel>My Account</DropdownMenuLabel>
-      <DropdownMenuItem>Profile</DropdownMenuItem>
-      <DropdownMenuItem>Billing</DropdownMenuItem>
-    </DropdownMenuGroup>
-    <DropdownMenuGroup>
-      <DropdownMenuSeparator />
-      <DropdownMenuItem>Team</DropdownMenuItem>
-      <DropdownMenuItem>Subscription</DropdownMenuItem>
-    </DropdownMenuGroup>
-  </DropdownMenuContent>
-</DropdownMenu>
-    </>
-    
-  )
+  
 }
 
 export default page
