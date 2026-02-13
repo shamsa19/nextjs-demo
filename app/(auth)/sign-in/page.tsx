ea2b256b6ce90f63bc2d7bca8d3b91e715362049
Sham Alahmad <112821170+shamsa19@users.noreply.github.com>
2026-02-13 21:41:11 +0300

    Add AuthForm, UI primitives & validations

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,20 @@
+"use client"
+import AuthForm from '@/components/forms/AuthForm'
+import { SignInSchema } from '@/lib/validations'
 import React from 'react'
+import { success } from 'zod'
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <AuthForm 
+    formType="SIGN_IN"
+    schema={SignInSchema}
+    defaultValues={{
+      email: '',
+      password: '',
+    }}
+    onSubmit={(data) => Promise.resolve({success: true, data})}
+    />
   )
 }
 
