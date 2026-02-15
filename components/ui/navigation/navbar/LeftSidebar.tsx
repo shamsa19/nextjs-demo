91892411c74b47039157ed41a2966ed2659acc49
Sham Alahmad <112821170+shamsa19@users.noreply.github.com>
2026-02-15 22:31:04 +0300

    Add conditional auth buttons to LeftSidebar

--- components/ui/navigation/navbar/LeftSidebar.tsx
+++ components/ui/navigation/navbar/LeftSidebar.tsx
@@ -11,43 +11,58 @@ import { NavLinks } from './NavLinks'
 import ROUTES from '@/constants/routes';
 import { Button } from '../../button';
 
+
 const LeftSidebar = () => {
+     const pathname = usePathname();
+     const userId = 1; 
+
+    const isLoggedIn = pathname === ROUTES.SIGN_IN || pathname === ROUTES.SIGN_UP;
+
     return (
-            <div className='flex pt-16 background-light900_dark200 border-none sticky px-3 '>
-                <Link
-                    href='/'
-                    className='flex items-center gap-1'
-                >
-                </Link>
-                <div className='no-scrollbar flex h-[calc(100vh-80px)] flex-col justify-between  overflow-y-auto'>
-                    <div>
-                        <section className='flex flex-col justify-between gap-6 pt-16'>
-                            <NavLinks />
-                        </section>
-                    </div>
-
-                    <div className='flex flex-col gap-3 '>
-                        <div>
-                            <Link href={ROUTES.SIGN_IN}>
-                            <Button className='small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none'>
-                                <span className='primary-text-gradient'>Log In</span>
-                            </Button>
-                            </Link>
-                            </div>
-                             <div>
-                            <Link href={ROUTES.SIGN_UP}>
-                            <Button className='small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none'>
-                                Sign Up
-                            </Button>
-                            </Link>
-                            </div>
-
-                    </div>
+        <div className='flex pt-16 background-light900_dark200 border-none no-scrollbar sticky px-3 '>
+            <Link
+                href='/'
+                className='flex items-center gap-1'
+            >
+            </Link>
+            <div className='no-scrollbar flex h-[calc(100vh-80px)] flex-col justify-between  overflow-y-auto'>
+                <div>
+                    <section className='flex flex-col justify-between gap-6 pt-16'>
+                        <NavLinks />
+                    </section>
                 </div>
 
-            </div>
+                <div className='flex flex-col gap-3 '>
+                    {userId ? (
+                        <Link href={ROUTES.SIGN_UP} className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                            Logout
+                        </Link>
+                    ) : (
+                        !isLoggedIn && (
+                        <>
+                            <Link href={ROUTES.SIGN_IN}>
+                                <Button className='small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none'>
+                                    <span className='primary-text-gradient'>Log In</span>
+                                </Button>
+                            </Link>
+
+
+                            <Link href={ROUTES.SIGN_UP}>
+                                <Button className='small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none'>
+                                    Sign Up
+                                </Button>
+                            </Link>
+
+
+
+                        </>
+                        )
+                    )}
+               
+                </div>
+                </div>
+                </div>
     )
 }
 
-
-export default LeftSidebar
+                export default LeftSidebar
